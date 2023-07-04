6113e068ac10b77d2d65e4ca60242dc3f6ca52b9
sudi050 <sudi050@gmail.com>
2023-07-04 15:31:48 +0530

    js animation enabled for choosing travel modes

--- bequem_voyage/static/js/index.js
+++ bequem_voyage/static/js/index.js
@@ -6,3 +6,14 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   } 
+
+  function swap(option) {
+    const cars = document.querySelectorAll('.car');
+    cars.forEach((car) => {
+        car.classList.remove('selected');
+    });
+
+    const selectedCar = document.getElementById(option);
+    selectedCar.classList.add('selected');
+    document.getElementById('mode').innerHTML = option;
+}
