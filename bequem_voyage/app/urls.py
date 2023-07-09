37c7d2dc408eec24c7794f0ea6d49f857f21f705
ADITHYA S NAIR <adithyasnair2021@gmail.com>
2023-07-10 02:00:51 +0530

    File updates
    Everything almost done

--- bequem_voyage/app/urls.py
+++ bequem_voyage/app/urls.py
@@ -6,7 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path("signup/", views.signup, name="signup"),
-    path("login/", views.login, name="login"),
+    path('', views.login, name="login"),
+    path("pricing/", views.pricing, name="pricing"),
+    path("cars/", views.cars, name="cars"),
+    path("profile/", views.profile, name="profile"),
+    path('profile/<str:driver>/', views.profile, name='profile'),
+    path("booking/", views.booking, name="booking"),
+    path("restaurant/", views.restaurant, name="restaurant"),
+
 ]
